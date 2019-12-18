d2eefbd4e73f5e1a083a8988a956c430e5e04986
picost <pierrecostini314@gmail.com>
2019-12-18 14:58:26 +0100

    #1

---
+++ cifar_model/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 18 09:39:15 2019
+
+@author: picost
+"""
+
+from . import dog_classifier
---
+++ cifar_model/dog_classifier.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 18 09:39:30 2019
+
+@author: picost
+"""
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+# define the CNN architecture
+class Net(nn.Module):
+    def __init__(self, n_class, imsize):
+        super(Net, self).__init__()
+        assert (imsize % 8 == 0), "Imsize must be a divisible by 8"
+        # convolutional layer (sees 32x32x3 image tensor)
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        # convolutional layer (sees 16x16x16 tensor)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        # convolutional layer (sees 8x8x32 tensor)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        # max pooling layer
+        self.pool = nn.MaxPool2d(2, 2)
+        self.n_feat = int(imsize / 8)
+        # linear layer (64 * 10 * 10 -> 500)
+        self.fc1 = nn.Linear(64 * self.n_feat * self.n_feat, 500)
+        # linear layer (500 -> 10)
+        self.fc2 = nn.Linear(500, n_class)
+        # dropout layer (p=0.25)
+        self.dropout = nn.Dropout(0.25)
+
+    def forward(self, x):
+        # add sequence of convolutional and max pooling layers
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        # flatten image input
+        x = x.view(-1, 64 * self.n_feat * self.n_feat)
+        # add dropout layer
+        x = self.dropout(x)
+        # add 1st hidden layer, with relu activation function
+        x = F.relu(self.fc1(x))
+        # add dropout layer
+        x = self.dropout(x)
+        # add 2nd hidden layer, with relu activation function
+        x = self.fc2(x)
+        return x
